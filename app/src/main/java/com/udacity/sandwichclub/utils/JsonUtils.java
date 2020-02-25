e028afeb1f495cf300715fe768d8c040af90ae01
Emanuelle Rizzuto <bridoka@gmail.com>
2020-02-24 21:34:34 -0300

    Implement method parseSandwichJson. Manipulation of json to get data for create Sandwich object

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,46 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
-public class JsonUtils {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        String mainName = null;
+        String placeOfOrigin = null;
+        String description = null;
+        String image = null;
+        List<String> alsoKnownAs = new ArrayList<String>();
+        List<String> ingredients = new ArrayList<String>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject name = jsonObject.getJSONObject("name");
+            mainName = name.getString("mainName");
+            placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            description = jsonObject.getString("description");
+            image = jsonObject.getString("image");
+
+            JSONArray alsoKnownData = name.getJSONArray("alsoKnownAs");
+            for(int i = 0; i < alsoKnownData.length(); i++) {
+                alsoKnownAs.add(alsoKnownData.get(i).toString());
+            }
+
+            JSONArray ingredientsData = jsonObject.getJSONArray("ingredients");
+            for(int i = 0; i < ingredientsData.length(); i++) {
+                ingredients.add(ingredientsData.get(i).toString());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
     }
 }
